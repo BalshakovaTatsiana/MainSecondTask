ae1cea6ce0ee2b0a979d54a6f3cd409457e16cf1
Tatsiana Balshakova <balshakovatatsiana@gmail.com>
2020-04-15 18:57:23 +0300

    Add project files.

---
+++ MainSecodTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainSecodTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 20;
+            while(index > 0)
+            {
+                Console.WriteLine(index);
+                index--;
+            }
+        }
+    }
+}
